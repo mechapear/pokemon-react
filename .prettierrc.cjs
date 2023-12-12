238dc29b92d1a5586b9d77c8321915c683c39587
mechapear <mechapear@gmail.com>
2023-12-12 09:42:50 +0700

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  plugins: ['prettier-plugin-tailwindcss']
+}
