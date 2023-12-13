d53d099d0ead5916f04be4c12f64b70d48df3ced
mechapear <mechapear@gmail.com>
2023-12-13 17:24:48 +0700

    feat: handle search status

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Button from './modules/Button.tsx'
+import { ErrorIcon } from './modules/icons.tsx'
 import InfoCard from './modules/InfoCard.tsx'
 import SearchSection, { SearchSectionProps } from './modules/SearchSection.tsx'
 import StatsCard from './modules/StatsCard.tsx'
@@ -40,32 +41,42 @@ export type PokemonInfo = {
   }[]
 }
 
-async function fetchPokemon(endpoint: string) {
-  try {
-    const data = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${endpoint.toLowerCase()}/`,
-    ).then((response) => response.json() as Promise<PokemonInfo>)
-    return data
-  } catch (error) {
-    console.log(`Pokemon not found: `, error)
-  }
-}
-
 export default function App() {
   const [pokemon, setPokemon] = useState('')
   const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo | undefined>(
     undefined,
   )
   const [isMoreDetails, setIsMoreDetails] = useState(false)
+  const [status, setStatus] = useState('')
+
+  async function fetchPokemon(endpoint: string) {
+    try {
+      const data = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${endpoint.toLowerCase()}/`,
+      ).then((response) => response.json() as Promise<PokemonInfo>)
+      setStatus('success')
+      return data
+    } catch (error) {
+      console.log(`Pokemon not found: `, error)
+      setStatus('error')
+    }
+  }
 
   const handleSearchPokemon: SearchSectionProps['onFormSubmit'] = async (
     event,
   ) => {
     // prevent page refreshing
     event.preventDefault()
+    setStatus('loading')
 
     const pokemonInfo = await fetchPokemon(pokemon)
-    setPokemonInfo(pokemonInfo)
+    if (pokemonInfo === undefined) {
+      setStatus('error')
+      setPokemonInfo(undefined)
+    } else {
+      setStatus('success')
+      setPokemonInfo(pokemonInfo)
+    }
 
     // reset search input
     setPokemon('')
@@ -79,38 +90,63 @@ export default function App() {
     setPokemonInfo(pokemonInfo)
   }
 
+  console.log(`> status: `, status)
+
   return (
-    <>
-      <section className="my-10 flex min-h-screen flex-col items-center gap-3">
-        <h1 className="my-2 text-center text-3xl font-bold text-blue-700">
-          Find Your Pokémon
-        </h1>
-        <SearchSection
-          onFormSubmit={handleSearchPokemon}
-          onInputChange={(event) => {
-            setPokemon(event.target.value)
-          }}
-          value={pokemon}
-        />
-        <div className="my-2">
-          <Button onClick={handleRandomPokemon} label="Random Pokemon" />
+    <section className="my-10 flex min-h-screen flex-col items-center gap-3">
+      <h1 className="my-2 text-center text-3xl font-bold text-blue-700">
+        Find Your Pokémon
+      </h1>
+      <SearchSection
+        onFormSubmit={handleSearchPokemon}
+        onInputChange={(event) => {
+          setPokemon(event.target.value)
+        }}
+        value={pokemon}
+      />
+      <div className="my-2">
+        <Button onClick={handleRandomPokemon} label="Random Pokemon" />
+      </div>
+
+      {/*loading*/}
+      {status === 'loading' ? (
+        <div className="m-6">
+          <div
+            className="h-10 w-10 animate-spin rounded-full border-[6px] border-gray-300 border-t-blue-700"
+            aria-label="loading icon"
+          />
         </div>
-        {pokemonInfo ? (
-          <>
-            <InfoCard pokemonInfo={pokemonInfo} />
-            <div className="my-2">
-              <Button
-                onClick={() => setIsMoreDetails((prevOption) => !prevOption)}
-                label={isMoreDetails ? 'Hide Details' : 'Show Details'}
-              />
-            </div>
-          </>
-        ) : null}
-        {pokemonInfo && isMoreDetails ? (
-          <StatsCard pokemonInfo={pokemonInfo} />
-        ) : null}
-      </section>
-    </>
+      ) : null}
+
+      {/*success*/}
+      {status === 'success' && pokemonInfo ? (
+        <>
+          <InfoCard pokemonInfo={pokemonInfo} />
+          <div className="my-2">
+            <Button
+              onClick={() => setIsMoreDetails((prevOption) => !prevOption)}
+              label={isMoreDetails ? 'Hide Details' : 'Show Details'}
+            />
+          </div>
+        </>
+      ) : null}
+      {pokemonInfo && isMoreDetails ? (
+        <StatsCard pokemonInfo={pokemonInfo} />
+      ) : null}
+
+      {/*error*/}
+      {status === 'error' ? (
+        <div className="m-6 grid w-[250px] place-items-center gap-3 rounded-lg border p-6 text-center text-red-500 shadow-sm">
+          <ErrorIcon />
+          <p className="text-sm text-blue-700">
+            Oops! Pokemon not found. Please try again
+          </p>
+          <div className="mt-2">
+            <Button onClick={() => setStatus('')} label={'Try again'} />
+          </div>
+        </div>
+      ) : null}
+    </section>
   )
 }
 
